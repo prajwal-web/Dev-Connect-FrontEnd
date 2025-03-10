74b53dba43d35cdaeb88204240de5e98616d2671
Prajwal V <prajuu2000@gmail.com>
2025-03-10 10:38:59 +0530

    fndjnf

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <>
-      <h1>hi prajwal</h1>
-      {/* https://dev-connect-service.onrender.com/ */}
-    </>
-  );
+  return <>{/* https://dev-connect-service.onrender.com/ */}</>;
 }
 
 export default App;
